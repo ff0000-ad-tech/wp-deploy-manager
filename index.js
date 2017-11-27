9f112006d6dce75598a5d3b64e8028df55247b74
Greg Connell <greg.connell@ff0000.com>
2017-11-26 23:46:43 -0800

    refactor beta working

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
-const _ = require('lodash');
-const path = require('path');
-
-const profile = require('./lib/profile.js');
-const adSettings = require('./lib/ad-settings.js');
-const adEnvironments = require('./lib/ad-environments.js');
+const Deploy = require('./lib/deploy/deploy.js');
+const Ad = require('./lib/ad/ad.js');
+const Payload = require('./lib/payload/payload.js');
 
 const debug = require('debug');
-var log = debug('deploy-manager');
+var log = debug('DM');
 
+// deploy
+var deploy = Deploy;
 
-// build the deploy-profile model
-function init(deploy) {
-	// resolve all of the [deploy.model.ref] paths
-	deploy = profile.conform(deploy);
+// ad
+var ad = Ad;
+ad.init(deploy);
 
-	// read the latest state into the deploy model
-	refresh(deploy);
-
-	log('Deploy Profile:');
-	log(deploy);
-	return deploy;
-}
+// payload
+var payload = Payload;
 
 
 
-
-// generate `deploy.ad` and `deploy.env`
-function refresh(deploy) {
-	log('refresh()');
-	// refresh ad-settings
-	deploy.ad = adSettings.refresh(deploy);
-	log(deploy.ad.assets);
-
-	// refresh ad-environments
-	deploy.env = adEnvironments.refresh(deploy);
-}
-
-
-
-
-module.exports = { 
-	init,
-	refresh
+module.exports = {
+	deploy,
+	ad,
+	payload
 };
