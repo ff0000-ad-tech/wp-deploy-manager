663cfae0d4320393490d3a9186ba8195a3b2af71
Greg Connell <greg.connell@ff0000.com>
2017-12-07 15:10:08 -0800

    repathing for index-centric deploys

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const Deploy = require('./lib/deploy/deploy.js');
-const Ad = require('./lib/ad/ad.js');
-const Payload = require('./lib/payload/payload.js');
+const Deploy = require('./lib/deploy/deploy.js')
+const Ad = require('./lib/ad/ad.js')
+const Payload = require('./lib/payload/payload.js')
 
-const debug = require('debug');
-var log = debug('DM');
+const debug = require('debug')
+var log = debug('DM')
 
 // deploy
-var deploy = Deploy;
+var deploy = Deploy
 
 // ad
-var ad = Ad;
-ad.init(deploy);
+var ad = Ad
+ad.init(deploy)
 
 // payload
-var payload = Payload;
-
-
+var payload = Payload
 
 module.exports = {
 	deploy,
 	ad,
 	payload
-};
+}
