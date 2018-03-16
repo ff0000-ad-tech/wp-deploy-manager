3b908c5cc7985dd2c8f7d08abae6cf1a3c80b4e9
Derek Miranda <derek.miranda@ff0000.com>
2018-03-16 12:20:21 -0700

    Hooked up config utilities to exports

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Deploy = require('./lib/deploy/deploy.js')
 const Ad = require('./lib/ad/ad.js')
 const Payload = require('./lib/payload/payload.js')
 const Aliases = require('./lib/utils/aliases.js')
+const Config = require('./lib/utils/config.js')
 
 const debug = require('debug')
 var log = debug('DM')
@@ -19,9 +20,13 @@ var payload = Payload
 // alias management
 var aliases = Aliases
 
+// config utilities
+var config = Config
+
 module.exports = {
 	deploy,
 	ad,
 	payload,
 	aliases,
+	config
 }
