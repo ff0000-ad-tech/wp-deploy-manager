3c94e6540409171a063382c0e32abab74b916450
Derek Miranda <derek.miranda@ff0000.com>
2018-03-19 10:52:52 -0700

    Split up config utils among Babel and Plugins modules

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Deploy = require('./lib/deploy/deploy.js')
 const Ad = require('./lib/ad/ad.js')
 const Payload = require('./lib/payload/payload.js')
 const Aliases = require('./lib/utils/aliases.js')
-const Config = require('./lib/utils/config.js')
+const Plugins = require('./lib/plugins')
+const Babel = require('./lib/babel')
 
 const debug = require('debug')
 var log = debug('DM')
@@ -20,13 +21,17 @@ var payload = Payload
 // alias management
 var aliases = Aliases
 
-// config utilities
-var config = Config
+// base Webpack loaders for debug and production settings
+var babel = Babel
+
+// base Webpack config plugins
+var plugins = Plugins
 
 module.exports = {
 	deploy,
 	ad,
 	payload,
 	aliases,
-	config
+	babel,
+	plugins
 }
